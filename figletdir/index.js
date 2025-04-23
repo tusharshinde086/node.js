d42eb6592f079cf153aaaf0c928014549de0f606
Tushar Shinde <tusharshindets086@gmail.com>
2025-04-23 20:38:21 +0530

    figlet hello world

--- figletdir/index.js
+++ figletdir/index.js
@@ -1,1 +1,20 @@
 const figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
+
+//   Output :
+//   $ node index.js
+//     _   _      _ _        __        __         _     _ _ _
+//    | | | | ___| | | ___   \ \      / /__  _ __| | __| | | |
+//    | |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _` | | |
+//    |  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|_|
+//    |_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_|_)
+  
+  
