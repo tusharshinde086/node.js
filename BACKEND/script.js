8255b377d2872b131e262292dd3660199d077c29
Tushar Shinde <tusharshindets086@gmail.com>
2025-04-23 17:33:59 +0530

    process.argv

--- BACKEND/script.js
+++ BACKEND/script.js
@@ -3,3 +3,12 @@ for (let i=0;i<n;i++){
     console.log("hello, ",i);
 }
 console.log("bye!");
+
+//process
+
+//   process.realease              , process.cwd() ,
+console.log(process.argv);
+let args = process.argv;
+for(let i =2;i<args.length; i++){
+    console.log("hello to ",args[i])
+}
