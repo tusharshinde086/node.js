d99d7082d516a8163dbfc5ba618101d2aa4e8fcf
Tushar Shinde <tusharshindets086@gmail.com>
2025-04-24 10:56:51 +0530

    used import - require

--- BACKEND/export_script.js
+++
@@ -1,4 +0,0 @@
-
-import {add , time} from"./export.js";
-
-console.log(add(1,4));
---
+++ BACKEND/import_scriptgit .js
@@ -0,0 +1,7 @@
+import {add , time} from"./export.js";
+// import
+console.log(add(1,4));
+console.log("_-------------------------------")
+
+import {generate} from "random-words";
+console.log(generate());
